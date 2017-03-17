6b396082adb6f9bc6cfe9c180d04d186a9e6d9dc
luis1415 <luisbermudeztorres@gmail.com>
2017-03-17 12:16:49 -0500

    Pronostico Base

---
+++ application/views/template/footer.php
@@ -0,0 +1,24 @@
+<!--FOOTER -->
+<div width="100%" height="10%" id="footer" style="background-color:#000000; color: white; align: center;">
+    <table border="0" border="0" width="100%">
+        <tr style="padding-bottom:2px;" align="center">
+            <td> Sistema de Alerta Temprana de Medellín y el valle de Aburrá</td>
+        </tr>
+        <tr style="padding-bottom:2px;" align="center">
+            <td> Calle 50 # 71-147- Torre SIATA </td>
+        </tr>
+        <tr style="padding-bottom:2px;" align="center">
+            <td> Tel&eacute;fonos: 4341987- 4341993 </td>
+        </tr>
+        <tr align="center">
+            <td> monitoreo.siata@gmail.com </td>
+        </tr>
+        <tr align="center">
+            <td> 2017 </td>
+        </tr>
+    </table>
+</div>
+
+</body>
+</html>
+<!--END FOOTER -->
---
+++ application/views/template/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Pronóstico</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?php echo base_url(); ?>favicon.ico" type="image/gif">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href=""> </a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>imagenes/icono.jpg" height="32" width="42"></a></li>
+            <li><a href="#">Pronóstico</a></li>
+            <li><a href="#">Forecast</a></li>
+            <li><a href="#">Acerca del Pronóstico</a></li>
+        </ul>
+    </div>
+</nav>
